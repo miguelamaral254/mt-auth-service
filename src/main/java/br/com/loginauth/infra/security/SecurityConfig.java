c53f74aeab4bb91e8900ebeb5cf266969a1026c3
Miguel <miguel.amaral.sales@gmail.com>
2024-10-01 22:54:32 -0300

    update

--- src/main/java/br/com/loginauth/infra/security/SecurityConfig.java
+++ src/main/java/br/com/loginauth/infra/security/SecurityConfig.java
@@ -102,7 +102,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://mediotec-plus.vercel.app"));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));  // Cabeçalhos permitidos
