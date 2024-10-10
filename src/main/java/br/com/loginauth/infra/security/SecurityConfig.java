aeb94911b941f35dc42f1308ac0a6f0ccc576c00
Miguel <miguel.amaral.sales@gmail.com>
2024-10-10 15:54:52 -0300

    update

--- src/main/java/br/com/loginauth/infra/security/SecurityConfig.java
+++ src/main/java/br/com/loginauth/infra/security/SecurityConfig.java
@@ -113,7 +113,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://mediotec-plus.vercel.app","https://mediotec-frontend.vercel.app", "https://mediotec-plus-atd051rza-miguelamaral254s-projects.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://mediotec-plus.vercel.app",
+                "https://mediotec-frontend.vercel.app",
+                "https://mediotec-plus-atd051rza-miguelamaral254s-projects.vercel.app"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
